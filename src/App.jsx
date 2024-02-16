dad587d5e3d4f49e9e21777ab8e5e01a85121154
SaagarRaj <saagartiwari1992@gmail.com>
2024-02-16 11:56:30 -0800

    Initialized project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold ">
+        Real time hand gesture recognition
+      </h1>
+    </>
+  );
+}
+
+export default App;
